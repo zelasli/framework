b518019bcaf649948c994c6401f4fa579f6f6720
Rufai Limantawa <rufailimantawa@gmail.com>
2024-02-11 15:54:22 +0100

    Initial framework configuration components with its dependencies. Now resolved config (settings and routing)
    The settings variables used by framework to the project apps and
    routing for using http request URI to get destination controller callback.

--- src/FrameworkKernel.php
+++ src/FrameworkKernel.php
@@ -9,6 +9,7 @@
 
 namespace Zelasli\Core;
 
+use Composer\Autoload\ClassLoader;
 use Zelasli\Container\ContainerInterface as IContainer;
 use Zelasli\Debugger\Engine as Debugger;
 use Zelasli\Routing\RouteBuilder;
@@ -33,6 +34,33 @@ final class FrameworkKernel implements IContainer
         $this->bindings = [];
         $this->aliases = [];
         $this->components = [];
+    }
+
+    /**
+     * Get configurations and setup container
+     * 
+     * @return void
+     */
+    protected function configure(): void
+    {
+        global $loader;
+        $settings = $this->processSettings();
+        $modules = $settings['MODULES_NAMESPACE'];
+        
+        $debugger = $this->make('Debugger', $settings['DEBUG']);
+        $debugger->initialize();
+
+        if (isset($loader) && $loader instanceof ClassLoader) {
+            // Register module namespace not installed by composer
+            foreach ($modules as $namespace => $directory) {
+                $namespace = rtrim($namespace, '\\') . '\\';
+                
+                $loader->addPsr4($namespace, $directory);
+            }
+        }
+
+        $this->processRouting($settings['BASE_DIR']);
+
     }
 
     /**
@@ -62,5 +90,46 @@ final class FrameworkKernel implements IContainer
         $this->alias('Debugger', Debugger::class);
         $this->alias('RouteBuilder', RouteBuilder::class);
         $this->alias('Router', Router::class);
+
+        $this->configure();
+    }
+
+    /**
+     * Configure routing to the container
+     * 
+     * @param string $baseDir
+     * 
+     * @return void
+     */
+    public function processRouting($baseDir): void
+    {
+        if (is_file($routesFileDir = $baseDir . '/conf/routes.php')) {
+            $routeBuilder = $this->make('RouteBuilder');
+    
+            require_once $routesFileDir;
+        }
+    }
+
+    /**
+     * Get conf settings
+     * 
+     * @return <string, mixed>
+     */
+    protected function processSettings(): array
+    {
+        static $set = false;
+        static $settings = [];
+
+        if (!$set) {
+            if (file_exists($file = dirname(__DIR__, 4) . '/conf/settings.php')) {
+                $set = true;
+
+                include_once $file;
+
+                $settings += $config;
+            }
+        }
+
+        return $settings;
     }
 }
